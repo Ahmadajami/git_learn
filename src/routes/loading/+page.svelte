7a65101899054363a90d6a9c5935c3963d42a254
Ahmad Alajami <ahmad_2000_aj@hotmail.com>
2024-08-13 15:02:37 +0300

    Loading Route Added

---
+++ src/routes/loading/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+	let arr = data.loading;
+</script>
+
+<h1>By Using this Route</h1>
+<main>
+	{#each arr as val}
+		<h6>Loading{val}</h6>
+	{/each}
+	<h1>Finish Loading</h1>
+</main>
